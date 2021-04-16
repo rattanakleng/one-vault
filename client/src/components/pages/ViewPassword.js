26a1a850a3a5b5a76564c6b49274c8e542f9021f
Rattanak Leng <rleng@live.com>
2021-04-15 17:58:38 -0700

    ViewPassword.js-add page title

--- client/src/components/pages/ViewPassword.js
+++ client/src/components/pages/ViewPassword.js
@@ -5,7 +5,8 @@ import PasswordFilter from '../passwords/PasswordFilter';
 const Home = () => {
   return (
       <div className="min-vh-100 mt-0 bg-beau-blue pb-5">
-        <div className="container pt-5">
+        <div className="container pt-5 px-5">
+        <h3 className="text-center mb-5">All Passwords</h3>
 
         <PasswordFilter />
   
