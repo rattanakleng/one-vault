dc78e31c3734d4ce2358134aec8dc2c177ab9fb2
Rattanak Leng <rleng@live.com>
2021-04-26 17:28:03 -0700

    pepare for deploy

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
+  // get everything, then direct to client, build, index.html
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
